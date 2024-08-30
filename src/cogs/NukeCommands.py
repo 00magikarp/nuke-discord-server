7849367063ca439abf4d9ad6a366f64b608cedeb
00magikarp <94652654+00magikarp@users.noreply.github.com>
2024-08-29 20:53:35 -0400

    Merge 'nukeall' intoo 'dev'

--- src/cogs/NukeCommands.py
+++ src/cogs/NukeCommands.py
@@ -78,6 +78,33 @@ class NukeCommands(commands.GroupCog, name="nuke", description="Nuke commands"):
 
         await interaction.response.send_message("💥", ephemeral=True)
 
+    @discord.app_commands.command(
+        name="all",
+        description="Nuke EVERYTHING! (channels, roles, users; this is not a joke!)"
+    )
+    async def all(self, interaction: discord.Interaction) -> None:
+        if not await self.check_admin(interaction):
+            return
+
+        for channel in interaction.guild.channels:
+            try:
+                await channel.delete()
+            except Exception as e:
+                print(e)
+
+        for role in interaction.guild.roles:
+            if not (role.permissions.administrator or role.name == "@everyone"):
+                try:
+                    await role.delete()
+                except Exception as e:
+                    print(e)
+
+        for user in interaction.guild.members:
+            if not user.guild_permissions.administrator:
+                await user.ban()
+
+
+
 
 async def setup(bot):
     await bot.add_cog(NukeCommands(bot))
