b5be2996edafd5ae64305175972b125a919fb420
00magikarp <94652654+00magikarp@users.noreply.github.com>
2024-10-11 18:07:25 -0400

    1.2.2
    Made logger less verbose

--- extras/CustomLogger.py
+++ extras/CustomLogger.py
@@ -19,7 +19,7 @@ class CustomLogger:
 
     def __init__(self):
         self.logger = logging.getLogger('discord')
-        self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.INFO)
 
         handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
         handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s: %(name)s: %(message)s'))
